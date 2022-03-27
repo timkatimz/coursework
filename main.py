2ff95a7576a545296dae93eeb2d40d9cc855a4e5
tim <timrhymer@ya.ru>
2022-03-27 01:40:43 -0400

    создал файлы main.py и utils.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+
+
+
+app.run(debug=True)
