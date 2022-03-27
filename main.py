d69cab60cc3f55e9a1a9f2a14f781553e4c7b08f
tim <timrhymer@ya.ru>
2022-03-27 13:57:56 -0400

    сделал вьюшки (/, /posts/<postid>, /search)

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from flask import Flask, request, render_template
+from utils import load_data, get_post, show_comments, search
+
 
 app = Flask(__name__)
+data = load_data()
 
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 
+@app.route("/posts/<int:post_id>")         # не отображается значок глаза, неправильные пути
+def show_post(post_id):
+    post = get_post(post_id)
+    comments = show_comments(post_id)
+    return render_template("post.html", post=post, comments=comments)
 
+
+@app.route("/search/")  # пока не работает
+def search_by_name():
+    search_key = request.args.get("search")
+    posts = search(search_key)
+    return render_template("search.html", posts=posts, search_key=search_key)
 
 app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,34 @@
+import json
+
+
+def load_data():
+    with open("data/data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        return data
+
+def get_post(post_id):
+    for post in load_data():
+        if post_id == post["pk"]:
+            return post
+
+
+def load_comments():
+    with open("data/comments.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        return data
+
+
+def show_comments(post_id):
+    comments = []
+    for comment in load_comments():
+        if post_id == comment["post_id"]:
+            comments.append(comment)
+    return comments
+
+
+def search(search_key):
+    found_posts = []
+    for post in load_data():
+        if search_key in post["content"] and len(search_key) > 0:
+            found_posts.append(post)
+    return found_posts
